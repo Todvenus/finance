ebce1079de6a6846300783241fc6445fd7a1578d
Tod <todvenus@gmail.com>
2023-04-13 18:34:51 +0800

    css animation & created finance javacript section

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
 //Дэлгэцтэй ажиллах 
 var uiController = (function(){
 
+    var DOMstring = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputAddBtn: ".add__btn"
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstring.inputType).value,
+                description: document.querySelector(DOMstring.inputDescription).value,
+                vale: document.querySelector(DOMstring.inputValue).value,
+               
+            };
+        },
+
+        getDOMstring:  function(){
+            return DOMstring;
+        }
+    };
+
 })();
 // Санхүүтэй ажиллах
 var financeController = (function(){
 
+    var Income = function(id, description, value){
+        this.id = id,
+        this.description = description,
+        this.value = value;
+    };
+
+    var Expence = function(id, description, value){
+        this.id = id,
+        this.description = description,
+        this.value = value;
+    };
+    
+    var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    }
+
 })();
 // Програмын холбогч
 var appController = (function(uiCtrl, fnCtrl){
-
     var ctrlAddItem = function(){
-        console.log('Section for getting data from display');
+        console.log(uiController.getInput());
 
     };
 
-    document.querySelector('.add__btn').addEventListener('click', function(){
+var setupEventListeners = function (){
+    var DOM = uiController.getDOMstring();
+
+    document.querySelector(DOM.inputAddBtn).addEventListener('click', function(){
         ctrlAddItem();
     });
 
     document.addEventListener('keypress' , function(event){
-        if(event.Keycode === 13 |  event.which === 13) {
+        if(event.Keycode === 13 |  UIEvent.which === 13) {
             ctrlAddItem();
         }
     });
+}; 
 
-   document.querySelector('.add__btn').addEventListener('click', function(){
-    
-   });      
-    
+return {
+    init: function() {
+        console.log('Starting...');
+        setupEventListeners();
+    }
+};
+   
 })(uiController, financeController); 
+
+appController.init();
